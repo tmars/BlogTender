c6aade512e7850a8827764123c3f34946ab22ec5
Marseille <t.mars@mail.ru>
2013-09-18 17:03:29 +0400

    changed fixtures loader

--- src/mh/BTBundle/DataFixtures/ORM/FixtureLoader.php
+++ src/mh/BTBundle/DataFixtures/ORM/FixtureLoader.php
@@ -6,42 +6,18 @@ use Doctrine\Common\Persistence\ObjectManager;
 
 use mh\BTBundle\Entity as Entity;
 
-define('DATA_DIR', __DIR__.'/../Data/');
-define('QUERY_DIR', __DIR__.'/../Query/');
-
 class FixtureLoader implements FixtureInterface
 {
+	const DATA_DIR = '/../Data/';
+	
     public function load(ObjectManager $manager)
     {
-        $this->execRawQueries($manager);
-
         $themes = $this->getThemes();
         $categories = $this->getCategories();
-        $users = $this->getUsers(20);
 
         $this->persistArray($manager, $themes);
         $this->persistArray($manager, $categories);
-        $this->persistArray($manager, $users);
         $manager->flush();
-    }
-
-    private function execRawQueries(ObjectManager $manager)
-    {
-        $c = $manager->getConnection();
-        $l = mysql_connect($c->getHost(), $c->getUsername(), $c->getPassword());
-        mysql_select_db($c->getDatabase());
-
-        $d = dir(QUERY_DIR);
-        while (false !== ($file = $d->read())) {
-            if ($file != "." && $file != "..") {
-                $query = file_get_contents(QUERY_DIR.$file);
-                var_dump(mysql_query($query));
-                var_dump(mysql_error());
-            }
-        }
-        $d->close();
-
-        mysql_close($l);
     }
 
     private function persistArray(ObjectManager $manager, array $data)
@@ -55,7 +31,7 @@ class FixtureLoader implements FixtureInterface
     {
         $themes = array();
 
-        $titles = file(DATA_DIR."themes.txt");
+        $titles = file(__DIR__.self::DATA_DIR."themes.txt");
         foreach ($titles as $title) {
             $t = new Entity\ThemeForPost();
             $t->setTitle($title);
@@ -69,7 +45,7 @@ class FixtureLoader implements FixtureInterface
     {
         $categories = array();
 
-        $labels = file(DATA_DIR."categories.txt");
+        $labels = file(__DIR__.self::DATA_DIR."categories.txt");
         foreach ($labels as $label) {
             $l = new Entity\Category();
             $l->setLabel($label);
