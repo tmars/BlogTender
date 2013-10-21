06af12b9c28ba713b267e1a10994c6ab998c017c
Marseille Talipov <t.mars@mail.ru>
2013-10-22 00:10:29 +0300

    added database changes

--- src/mh/BTBundle/Command/PatcherCommand.php
+++ src/mh/BTBundle/Command/PatcherCommand.php
@@ -8,6 +8,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use mh\Common\Slug;
+use mh\BTBundle\Entity as Entity;
 
 class PatcherCommand extends DoctrineCommand
 {
@@ -23,13 +24,39 @@ class PatcherCommand extends DoctrineCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
 		$em = $this->getEntityManager(null);
-		$categories = $em->getRepository('BTBundle:Category')->findAll();
+
+        /*$categories = $em->getRepository('BTBundle:Category')->findAll();
 		
 		foreach ($categories as $category) {
 			$slug = Slug::getSlug($category->getLabel());
 			echo $slug."\r\n";
 			$category->setSlug($slug);
-		}
+		}*/
+
+        $posts = $em->getRepository('BTBundle:Post')->findAll();
+        $answers = $em->getRepository('BTBundle:Answer')->findAll();
+        $questions = $em->getRepository('BTBundle:Question')->findAll();
+
+        $items = array(
+            'post' => $posts,
+            'answer' => $answers,
+            'question' => $questions,
+        );
+
+        foreach ($items as $mode => $vals) {
+            foreach ($vals as $item) {
+                if ($item->getScoreObject() === NULL) {
+                    $obj = new Entity\ScoreObject();
+                    $obj->setUser($item->getUser());
+                    $obj->setObjectType($mode);
+                    $obj->setScores(10);
+                    $obj->setCreatedDate($item->getCreatedDate());
+
+                    $item->setScoreObject($obj);
+                    $em->persist($obj);
+                }
+            }
+        }
 
 		$em->flush();
 	}
